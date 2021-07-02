080393d571b5b2b28743c5ab277dd73e2616af5a
faysal909 <faysalofficials909@gmail.com>
2021-07-02 18:38:22 +0600

    2nd commist

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ let point2 = Object.assign({},point)
 console.log(point)
 point2.x=200;
 console.log(point2)
+
+function test(a,b,c){
+    let sum = 0;
+    // console.log(arguments);
+    for(let i = 0; i<arguments.length;i++){   
+       sum+= arguments[i];       
+    }
+    let makeString = sum.toString();
+    console.log(makeString);
+}
+test(10,15);
+
